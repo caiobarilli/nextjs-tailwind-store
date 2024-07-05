7a3e0d6612aee01780333476ab42521422f3a3a0
Caio Barilli <caiobarilli@gmail.com>
2024-07-05 17:53:38 -0300

    fix: rename product type file

---
+++ src/lib/types/products.ts
@@ -0,0 +1,61 @@
+import { ColorProps, TagProps } from './filter'
+import { ReviewProps } from './reviews'
+
+export interface ProductsProps {
+  initialProducts: PagedProductsProps
+}
+
+export interface ProductProps {
+  id: number
+  name: string
+  price: number
+  quantity: number
+  description: string
+  additional_info: string
+  rating: number
+  slug: string
+  sku: string
+  reviews_ids: string
+  color_ids: string
+  tag_ids: string
+  cover: string
+  images: string
+}
+
+export interface SingleProductProps {
+  product: ProductProps
+  related_infos: RelatedInfoProductsProps
+  related_products: { products: ProductProps[] | [] }
+}
+
+export interface PagedProductsProps {
+  products: ProductProps[] | []
+  pagination: {
+    currentPage: number
+    totalPages: number
+  }
+}
+
+export interface ProductResumeWithColorsProps {
+  product: ProductProps
+  colors: ColorProps | ColorProps[]
+}
+
+export interface ProductInformationProps {
+  related_infos: RelatedInfoProductsProps
+  related_products: { products: ProductProps[] | [] }
+}
+
+export interface PaginationProps {
+  totalPages: number
+  currentPage: number
+}
+
+export interface RelatedInfoProductsProps {
+  description: string
+  additional_info: string
+  sku: string
+  reviews: ReviewProps | ReviewProps[] | undefined
+  colors: ColorProps | ColorProps[]
+  tags: TagProps | TagProps[]
+}
